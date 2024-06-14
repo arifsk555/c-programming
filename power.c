9987b345f32b1c3e4961ce65461dd6c2b4034124
arifsk555 <159996257+arifsk555@users.noreply.github.com>
2024-06-14 22:23:25 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int power(int base, int exp) {
+    if (exp == 0) {
+        return 1;
+    } else {
+        return base * power(base, exp - 1);
+    }
+}
+
+int main() {
+    int base, exp;
+    printf("Enter the base: ");
+    scanf("%d", &base);
+    printf("Enter the exponent: ");
+    scanf("%d", &exp);
+
+    printf("%d to the power of %d is %d\n", base, exp, power(base, exp));
+    return 0;
+}
