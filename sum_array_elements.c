d8f5cbacfdc360f1ca668f1207daeb9cadb93e91
arifsk555 <159996257+arifsk555@users.noreply.github.com>
2024-06-14 22:17:08 +0530

    Create sum_array_elements.c

---
+++ sum_array_elements.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int sumArray(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main() {
+    int n;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int sum = sumArray(arr, n);
+
+    printf("Sum of all array elements: %d\n", sum);
+
+    return 0;
+}
