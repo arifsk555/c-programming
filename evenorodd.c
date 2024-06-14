8db27714cb68efef1f88d2d981f4d7e6054c7db8
arifsk555 <159996257+arifsk555@users.noreply.github.com>
2024-06-14 23:20:19 +0530

    Create evenorodd.c

---
+++ evenorodd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0)
+        printf("%d is EVEN\n", num);
+    else
+        printf("%d is ODD\n", num);
+
+    return 0;
+}
