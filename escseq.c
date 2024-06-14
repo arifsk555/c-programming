7946fb9bdfa3c887e69171b06502afe40a2c313f
arifsk555 <159996257+arifsk555@users.noreply.github.com>
2024-06-14 23:19:53 +0530

    Create escseq.c

---
+++ escseq.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("New line example:\n");
+    printf("Tab\tExample\n");
+    printf("Backslash example: \\\n");
+    printf("Double quote example: \"Hello World\"\n");
+    printf("Single quote example: \'Hello World\'\n");
+
+    return 0;
+}
