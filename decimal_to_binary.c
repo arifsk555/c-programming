c75f82bb02d2bf98d4ede26db98ff146599f527f
arifsk555 <159996257+arifsk555@users.noreply.github.com>
2024-06-14 21:59:54 +0530

    Create decimal_to_binary.c

---
+++ decimal_to_binary.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void decimalToBinary(int num) {
+    if (num == 0) {
+        return;
+    }
+    decimalToBinary(num / 2);
+    printf("%d", num % 2);
+}
+
+int main() {
+    int num;
+    printf("Enter a decimal number: ");
+    scanf("%d", &num);
+
+    printf("Binary representation: ");
+    if (num == 0) {
+        printf("0");
+    } else {
+        decimalToBinary(num);
+    }
+    printf("\n");
+
+    return 0;
+}
