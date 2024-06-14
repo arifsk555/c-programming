2b6c167825d0b24f66ed196665fff1748976d67b
arifsk555 <159996257+arifsk555@users.noreply.github.com>
2024-06-14 22:58:21 +0530

    Create sum_of_natural.c

---
+++ sum_of_natural.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int N, sum = 0;
+    printf("Enter the value of N: ");
+    scanf("%d", &N);
+
+    for (int i = 1; i <= N; i++) {
+        sum += i;
+    }
+
+    printf("Sum of first %d natural numbers is %d\n", N, sum);
+    return 0;
+}
